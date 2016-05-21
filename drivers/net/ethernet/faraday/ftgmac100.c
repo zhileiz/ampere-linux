1223d9745ac5c3a55a4c68d01514f94282a8402e
Joel Stanley <joel@jms.id.au>
2016-05-22 00:47:15 +0930

    net/ethernet/faraday: Forward port ftgmac100 NC-SI
    This reflects changes made in order to build with 4.7.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/net/ethernet/faraday/ftgmac100.c
+++ drivers/net/ethernet/faraday/ftgmac100.c
@@ -1117,6 +1117,7 @@ static int ftgmac100_poll(struct napi_struct *napi, int budget)
 static int ftgmac100_open(struct net_device *netdev)
 {
 	struct ftgmac100 *priv = netdev_priv(netdev);
+	struct phy_device *phydev = netdev->phydev;
 	int err;
 
 	err = ftgmac100_alloc_buffers(priv);
@@ -1143,8 +1144,8 @@ static int ftgmac100_open(struct net_device *netdev)
 	ftgmac100_init_hw(priv);
 	ftgmac100_start_hw(priv, priv->use_ncsi ? 100 : 10);
 
-	if (priv->phydev)
-		phy_start(priv->phydev);
+	if (phydev)
+		phy_start(phydev);
 	else if (priv->use_ncsi)
 		netif_carrier_on(priv->netdev);
 
@@ -1178,6 +1179,7 @@ err_alloc:
 static int ftgmac100_stop_dev(struct net_device *netdev)
 {
 	struct ftgmac100 *priv = netdev_priv(netdev);
+	struct phy_device *phydev = netdev->phydev;
 
 	if (!priv->enabled)
 		return 0;
@@ -1188,8 +1190,8 @@ static int ftgmac100_stop_dev(struct net_device *netdev)
 
 	netif_stop_queue(netdev);
 	napi_disable(&priv->napi);
-	if (priv->phydev)
-		phy_stop(priv->phydev);
+	if (phydev)
+		phy_stop(phydev);
 
 	ftgmac100_stop_hw(priv);
 	free_irq(priv->irq, netdev);
@@ -1234,11 +1236,11 @@ static int ftgmac100_hard_start_xmit(struct sk_buff *skb,
 /* optional */
 static int ftgmac100_do_ioctl(struct net_device *netdev, struct ifreq *ifr, int cmd)
 {
-	struct ftgmac100 *priv = netdev_priv(netdev);
+	struct phy_device *phydev = netdev->phydev;
 
-	if (!priv->phydev)
+	if (!phydev)
 		return -EINVAL;
-	return phy_mii_ioctl(priv->phydev, ifr, cmd);
+	return phy_mii_ioctl(phydev, ifr, cmd);
 }
 
 static int ftgmac100_setup_mdio(struct ftgmac100 *priv)
@@ -1283,11 +1285,15 @@ err_alloc_mdiobus:
 	return err;
 }
 
-static void ftgmac100_destroy_mdio(struct ftgmac100 *priv)
+static void ftgmac100_destroy_mdio(struct net_device *netdev)
 {
-	if (!priv->use_ncsi)
+	struct ftgmac100 *priv = netdev_priv(netdev);
+	struct phy_device *phydev = netdev->phydev;
+
+	if (!phydev || !priv->use_ncsi)
 		return;
-	phy_disconnect(priv->phydev);
+
+	phy_disconnect(phydev);
 	mdiobus_unregister(priv->mii_bus);
 	mdiobus_free(priv->mii_bus);
 }
@@ -1342,7 +1348,7 @@ static int ftgmac100_probe(struct platform_device *pdev)
 	if (pdev->dev.of_node &&
 	    of_get_property(pdev->dev.of_node, "use-nc-si", NULL)) {
 		dev_info(&pdev->dev, "Using NCSI interface\n");
-		priv->phydev = NULL;
+		netdev->phydev = NULL;
 		priv->use_ncsi = true;
 	} else {
 		priv->use_ncsi = false;
@@ -1416,7 +1422,7 @@ static int ftgmac100_probe(struct platform_device *pdev)
 
 err_register_netdev:
 	if (!priv->use_ncsi)
-		ftgmac100_destroy_mdio(priv);
+		ftgmac100_destroy_mdio(netdev);
 	else
 		ncsi_unregister_dev(priv->ndev);
 err_ncsi_dev:
@@ -1440,7 +1446,7 @@ static int __exit ftgmac100_remove(struct platform_device *pdev)
 
 	unregister_netdev(netdev);
 
-	ftgmac100_destroy_mdio(priv);
+	ftgmac100_destroy_mdio(netdev);
 
 	iounmap(priv->base);
 	release_resource(priv->res);
