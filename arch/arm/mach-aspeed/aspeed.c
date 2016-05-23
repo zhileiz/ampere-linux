390339ad79232b11ebd532aa1ad09e29c99fddaf
Joel Stanley <joel@jms.id.au>
2016-05-23 23:03:28 +0930

    arm/aspeed: Add board file back in
    This is used for scu fixups. Will go away once we integrate pinmux.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>

---
+++ arch/arm/mach-aspeed/aspeed.c
@@ -0,0 +1,134 @@
+#include <linux/init.h>
+#include <linux/of_irq.h>
+#include <linux/of_platform.h>
+#include <linux/io.h>
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+
+#define AST_IO_VA	0xf0000000
+#define AST_IO_PA	0x1e600000
+#define AST_IO_SZ	0x00200000
+
+#define AST_IO(__pa)	((void __iomem *)(((__pa) & 0x001fffff) | AST_IO_VA))
+
+#define AST_BASE_LPC		0x1E789000 /* LPC Controller */
+#define AST_BASE_SPI		0x1E630000 /* SPI Memory Controller */
+#define AST_BASE_SCU		0x1E6E2000 /* System Control Unit (SCU) */
+#define AST_BASE_GPIO		0x1E780000 /* GPIO Controller */
+
+static struct map_desc aspeed_io_desc[] __initdata __maybe_unused = {
+	{
+		.virtual	=  AST_IO_VA,
+		.pfn		= __phys_to_pfn(AST_IO_PA),
+		.length		= AST_IO_SZ,
+		.type		= MT_DEVICE
+	},
+};
+
+static void __init do_common_setup(void)
+{
+	/* Enable LPC FWH cycles, Enable LPC to AHB bridge */
+	writel(0x00000500, AST_IO(AST_BASE_LPC | 0x80));
+
+	/* Flash controller */
+	writel(0x00000003, AST_IO(AST_BASE_SPI | 0x00));
+	writel(0x00002404, AST_IO(AST_BASE_SPI | 0x04));
+
+	/* Set UART routing */
+	writel(0x00000000, AST_IO(AST_BASE_LPC | 0x9c));
+
+	/* SCU setup
+	 *  - GPIOC{4,5,6} are FUNC_MODE{0,1,2}. These nets are
+	 *    connected to the Ethernet phy and to ensure the correct
+	 *    operation they all need to be in GPIO mode (SCU90[0] = 0)
+	 *    and then pulled down
+	 */
+	writel(0x01C000FF, AST_IO(AST_BASE_SCU | 0x88));
+	writel(0xC1C000FF, AST_IO(AST_BASE_SCU | 0x8c));
+	writel(0x003FA008, AST_IO(AST_BASE_SCU | 0x90));
+
+	/* Setup scratch registers */
+	writel(0x00000042, AST_IO(AST_BASE_LPC | 0x170));
+	writel(0x00004000, AST_IO(AST_BASE_LPC | 0x174));
+}
+
+static void __init do_barreleye_setup(void)
+{
+	u32 reg;
+
+	do_common_setup();
+
+	/* Setup PNOR address mapping for 64M flash */
+	writel(0x30000C00, AST_IO(AST_BASE_LPC | 0x88));
+	writel(0xFC0003FF, AST_IO(AST_BASE_LPC | 0x8C));
+
+	/* GPIO setup */
+	writel(0x9E82FCE7, AST_IO(AST_BASE_GPIO | 0x00));
+	writel(0x0370E677, AST_IO(AST_BASE_GPIO | 0x04));
+
+	/* SCU setup
+	 *  - GPION must be set to GPIO mode (SCU88[0:7] = 0) on
+	 *    Barreleye so they can be used to read the PCIe inventory
+	 *    status
+	 */
+
+	writel(0x01C00000, AST_IO(AST_BASE_SCU | 0x88));
+
+	/*
+	 * Do read/modify/write on power gpio to prevent resetting power on
+	 * reboot
+	 */
+	reg = readl(AST_IO(AST_BASE_GPIO | 0x20));
+	reg |= 0xCFC8F7FD;
+	writel(reg, AST_IO(AST_BASE_GPIO | 0x20));
+	writel(0xC738F20A, AST_IO(AST_BASE_GPIO | 0x24));
+	writel(0x0031FFAF, AST_IO(AST_BASE_GPIO | 0x80));
+}
+
+static void __init do_palmetto_setup(void)
+{
+	do_common_setup();
+
+	/* Setup PNOR address mapping for 32M flash */
+	writel(0x30000E00, AST_IO(AST_BASE_LPC | 0x88));
+	writel(0xFE0001FF, AST_IO(AST_BASE_LPC | 0x8C));
+
+	/* GPIO setup */
+	writel(0x13008CE7, AST_IO(AST_BASE_GPIO | 0x00));
+	writel(0x0370E677, AST_IO(AST_BASE_GPIO | 0x04));
+	writel(0xDF48F7FF, AST_IO(AST_BASE_GPIO | 0x20));
+	writel(0xC738F202, AST_IO(AST_BASE_GPIO | 0x24));
+
+	/* SCU setup */
+	writel(0x01C0007F, AST_IO(AST_BASE_SCU | 0x88));
+}
+
+#define SCU_PASSWORD	0x1688A8A8
+
+static void __init aspeed_init_early(void)
+{
+	// XXX UART stuff to fix to pinmux & co
+	writel(0x02010023, AST_IO(AST_BASE_LPC | 0x9c));
+	writel(SCU_PASSWORD, AST_IO(AST_BASE_SCU)); // UNLOCK SCU
+	writel(0xcb000000, AST_IO(AST_BASE_SCU | 0x80));
+	writel(0x00fff0c0, AST_IO(AST_BASE_SCU | 0x84));
+	writel(0x10CC5E80, AST_IO(AST_BASE_SCU | 0x0c));
+
+	/*
+	 * Ensure all IPs except GPIO and LPC are reset on watchdog expiry
+	 */
+	writel(0x001fdff3, AST_IO(AST_BASE_SCU | 0x9c));
+
+	/*
+	 * Temporary setup of AST registers until pinmux driver is complete
+	 */
+	if (of_machine_is_compatible("rackspace,barreleye-bmc"))
+		do_barreleye_setup();
+	if (of_machine_is_compatible("tyan,palmetto-bmc"))
+		do_palmetto_setup();
+}
+
+DT_MACHINE_START(aspeed_dt, "ASpeed SoC")
+	.init_early	= aspeed_init_early,
+MACHINE_END
