0aa00e9e023050e4e912d0112dfb6cfc1f2b8337
Joel Stanley <joel@jms.id.au>
2016-11-22 14:38:47 +1030

    arm: aspeed: Remove SCU8C hacks
    - ROM pins were configured but these are unused on Plametto.
    
    These settings were present, but I suspect unused:
    
     - GPIOR had the pulldown disabled. Most of these pins are unused and
       pulled up on Palmetto.
    
     - GPIOP is used as BMC_TACH, inputs from a fan controller.
    
     - GPIOI is the SPI buses for the flash.
    
     - GPIOH and G are misc I/O.
    
    Reviewed-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -45,9 +45,6 @@ static void __init do_common_setup(void)
 	/* Set UART routing */
 	writel(0x00000000, AST_IO(AST_BASE_LPC | 0x9c));
 
-	/* TODO: This should go in the GPIO driver device tree bindings */
-	writel(0xC1C000FF, AST_IO(AST_BASE_SCU | 0x8c));
-
 	/* Setup scratch registers */
 	writel(0x00000042, AST_IO(AST_BASE_LPC | 0x170));
 	writel(0x00008000, AST_IO(AST_BASE_LPC | 0x174));
