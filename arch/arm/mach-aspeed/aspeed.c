35a8ac3a340363647df803bea8718ad10ecdc44e
Rick Altherr via openbmc <openbmc@lists.ozlabs.org>
2017-01-31 17:12:30 -0800

    arm: aspeed: zaius: Disable LPC reset for UART1
    Currently, UART1 on Zaius BMC is unusable until brought out of reset by
    powering the host on. In that default reset state, ttyS0 can still be
    opened and UART1 silently drops bytes, which is not obviously expected
    behavior.
    
    Clear the LPC block control bit that enables LPCRST# as a reset source
    for UART1, making UART1 immediately useful in that it won't discard
    bytes.
    
    Signed-off-by: Xo Wang <xow@google.com>
    Reviewed-by: Andrew Jeffery <andrew@aj.id.au>
    [fixed comment formatting]
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -185,6 +185,14 @@ static void __init do_zaius_setup(void)
 
 	/* Set SPI1 CE0 decoding window to 0x30000000 */
 	writel(0x68600000, AST_IO(AST_BASE_SPI | 0x30));
+
+	/* Disable default behavior of UART1 being held in reset by LPCRST#.
+	 * By releasing UART1 from being controlled by LPC reset, it becomes
+	 * immediately available regardless of the host being up.
+	 */
+	reg = readl(AST_IO(AST_BASE_LPC | 0x98));
+	/* Clear "Enable UART1 reset source from LPC" */
+	writel(reg & ~BIT(4), AST_IO(AST_BASE_LPC | 0x98));
 }
 
 static void __init do_witherspoon_setup(void)
