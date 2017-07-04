990f06c35b7e119f00967e90f0514dbbd2e1b51b
Joel Stanley <joel@jms.id.au>
2017-07-04 10:33:08 +0930

    ARM: aspeed: Release UARTs from reset for all platforms
    This workaround was in place for Zaius. Make it common to all
    platforms as the held-in-reset behaviour breaks the Linux tty layer
    where users expect the existence of a device to mean it is usable.
    
    We release all four UARTs that are covered by this register from reset.
    
    OpenBMC-Staging-Count: 1
    Tested-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -173,14 +173,6 @@ static void __init do_zaius_setup(void)
 	/* Set pin high */
 	reg = readl(AST_IO(AST_BASE_GPIO | 0x00));
 	writel(reg | phy_reset_mask, AST_IO(AST_BASE_GPIO | 0x00));
-
-	/* Disable default behavior of UART1 being held in reset by LPCRST#.
-	 * By releasing UART1 from being controlled by LPC reset, it becomes
-	 * immediately available regardless of the host being up.
-	 */
-	reg = readl(AST_IO(AST_BASE_LPC | 0x98));
-	/* Clear "Enable UART1 reset source from LPC" */
-	writel(reg & ~BIT(4), AST_IO(AST_BASE_LPC | 0x98));
 }
 
 static void __init do_witherspoon_setup(void)
@@ -195,17 +187,7 @@ static void __init do_romulus_setup(void)
 
 static void __init do_lanyang_setup(void)
 {
-	unsigned long reg;
-
 	do_common_setup();
-
-	/* Disable default behavior of UART1 being held in reset by LPCRST#.
-	 * By releasing UART1 from being controlled by LPC reset, it becomes
-	 * immediately available regardless of the host being up.
-	 */
-	reg = readl(AST_IO(AST_BASE_LPC | 0x98));
-	/* Clear "Enable UART1 reset source from LPC" */
-	writel(reg & ~BIT(4), AST_IO(AST_BASE_LPC | 0x98));
 }
 
 static void __init do_mellanox_setup(void)
@@ -219,13 +201,6 @@ static void __init do_mellanox_setup(void)
 	reg |= BIT(7);
 	reg &= ~BIT(6);
 	writel(reg, AST_IO(AST_BASE_SCU | 0x70));
-
-	/* Disable default behavior of UART1 being held in reset by LPCRST#.
-	 * By releasing UART1 from being controlled by LPC reset, it becomes
-	 * immediately available regardless of the host being up. */
-	reg = readl(AST_IO(AST_BASE_LPC | 0x98));
-	/* Clear "Enable UART1 reset source from LPC" */
-	writel(reg & ~BIT(4), AST_IO(AST_BASE_LPC | 0x98));
 
 	/* Enable RMII1 50MHz RCLK output. */
 	reg = readl(AST_IO(AST_BASE_SCU | 0x48));
@@ -253,6 +228,14 @@ static void __init aspeed_init_early(void)
 	 * Ensure all IPs except GPIO and LPC are reset on watchdog expiry
 	 */
 	writel(0x001fdff3, AST_IO(AST_BASE_SCU | 0x9c));
+
+	/* Disable default behavior of UART1 being held in reset by LPCRST#.
+	 * By releasing UART1 from being controlled by LPC reset, it becomes
+	 * immediately available regardless of the host being up.
+	 */
+	reg = readl(AST_IO(AST_BASE_LPC | 0x98));
+	/* Clear "Enable UART reset source from LPC" for all four UARTs */
+	writel(reg & ~GENMASK(7, 4), AST_IO(AST_BASE_LPC | 0x98));
 
 	/*
 	 * Temporary setup of AST registers until pinmux driver is complete
