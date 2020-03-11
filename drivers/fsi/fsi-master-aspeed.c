c0a4870d768b3ac2f1d7efe36fe1b50f61581aae
Eddie James <eajames@linux.ibm.com>
2020-03-11 14:04:10 -0500

    fsi: aspeed: Enable 23-bit addressing
    In order to access more than the second hub link, 23-bit addressing is
    required. The core provides the highest two bits of address as the slave
    ID to the master.
    
    OpenBMC-Staging-Count: 1
    Signed-off-by: Eddie James <eajames@linux.ibm.com>
    Acked-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/fsi/fsi-master-aspeed.c
+++ drivers/fsi/fsi-master-aspeed.c
@@ -242,9 +242,10 @@ static int aspeed_master_read(struct fsi_master *master, int link,
 	struct fsi_master_aspeed *aspeed = to_fsi_master_aspeed(master);
 	int ret;
 
-	if (id != 0)
+	if (id > 0x3)
 		return -EINVAL;
 
+	addr |= id << 21;
 	addr += link * FSI_HUB_LINK_SIZE;
 
 	switch (size) {
@@ -274,9 +275,10 @@ static int aspeed_master_write(struct fsi_master *master, int link,
 	struct fsi_master_aspeed *aspeed = to_fsi_master_aspeed(master);
 	int ret;
 
-	if (id != 0)
+	if (id > 0x3)
 		return -EINVAL;
 
+	addr |= id << 21;
 	addr += link * FSI_HUB_LINK_SIZE;
 
 	switch (size) {
