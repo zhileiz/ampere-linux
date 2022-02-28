ea6eab296acc463f8fd8cf2a9fc48b229b81d803
Quan Nguyen <qnguyen@amperecomputing.com>
2022-02-28 13:06:59 +0700

    hwmon: (smpro-hwmon) Redefine smpro register name
    All registers are defined ending with _REG, remove this _REG postfix
    to improve readability.
    
    Signed-off-by: Quan Nguyen <qnguyen@amperecomputing.com>

--- drivers/hwmon/smpro-hwmon.c
+++ drivers/hwmon/smpro-hwmon.c
@@ -15,44 +15,44 @@
 #include <linux/regmap.h>
 
 /* Logical Power Sensor Registers */
-#define SOC_TEMP_REG			0x10
-#define SOC_VRD_TEMP_REG		0x11
-#define DIMM_VRD_TEMP_REG		0x12
-#define CORE_VRD_TEMP_REG		0x13
-#define CH0_DIMM_TEMP_REG		0x14
-#define CH1_DIMM_TEMP_REG		0x15
-#define CH2_DIMM_TEMP_REG		0x16
-#define CH3_DIMM_TEMP_REG		0x17
-#define CH4_DIMM_TEMP_REG		0x18
-#define CH5_DIMM_TEMP_REG		0x19
-#define CH6_DIMM_TEMP_REG		0x1A
-#define CH7_DIMM_TEMP_REG		0x1B
-#define RCA_VRD_TEMP_REG		0x1C
+#define SOC_TEMP		0x10
+#define SOC_VRD_TEMP		0x11
+#define DIMM_VRD_TEMP		0x12
+#define CORE_VRD_TEMP		0x13
+#define CH0_DIMM_TEMP		0x14
+#define CH1_DIMM_TEMP		0x15
+#define CH2_DIMM_TEMP		0x16
+#define CH3_DIMM_TEMP		0x17
+#define CH4_DIMM_TEMP		0x18
+#define CH5_DIMM_TEMP		0x19
+#define CH6_DIMM_TEMP		0x1A
+#define CH7_DIMM_TEMP		0x1B
+#define RCA_VRD_TEMP		0x1C
 
-#define CORE_VRD_PWR_REG		0x20
-#define SOC_PWR_REG			0x21
-#define DIMM_VRD1_PWR_REG		0x22
-#define DIMM_VRD2_PWR_REG		0x23
-#define CORE_VRD_PWR_MW_REG		0x26
-#define SOC_PWR_MW_REG			0x27
-#define DIMM_VRD1_PWR_MW_REG		0x28
-#define DIMM_VRD2_PWR_MW_REG		0x29
-#define RCA_VRD_PWR_REG			0x2A
-#define RCA_VRD_PWR_MW_REG		0x2B
+#define CORE_VRD_PWR		0x20
+#define SOC_PWR			0x21
+#define DIMM_VRD1_PWR		0x22
+#define DIMM_VRD2_PWR		0x23
+#define CORE_VRD_PWR_MW		0x26
+#define SOC_PWR_MW		0x27
+#define DIMM_VRD1_PWR_MW	0x28
+#define DIMM_VRD2_PWR_MW	0x29
+#define RCA_VRD_PWR		0x2A
+#define RCA_VRD_PWR_MW		0x2B
 
-#define MEM_HOT_THRESHOLD_REG		0x32
-#define SOC_VR_HOT_THRESHOLD_REG	0x33
-#define CORE_VRD_VOLT_REG		0x34
-#define SOC_VRD_VOLT_REG		0x35
-#define DIMM_VRD1_VOLT_REG		0x36
-#define DIMM_VRD2_VOLT_REG		0x37
-#define RCA_VRD_VOLT_REG		0x38
+#define MEM_HOT_THRESHOLD	0x32
+#define SOC_VR_HOT_THRESHOLD	0x33
+#define CORE_VRD_VOLT		0x34
+#define SOC_VRD_VOLT		0x35
+#define DIMM_VRD1_VOLT		0x36
+#define DIMM_VRD2_VOLT		0x37
+#define RCA_VRD_VOLT		0x38
 
-#define CORE_VRD_CURR_REG		0x39
-#define SOC_VRD_CURR_REG		0x3A
-#define DIMM_VRD1_CURR_REG		0x3B
-#define DIMM_VRD2_CURR_REG		0x3C
-#define RCA_VRD_CURR_REG		0x3D
+#define CORE_VRD_CURR		0x39
+#define SOC_VRD_CURR		0x3A
+#define DIMM_VRD1_CURR		0x3B
+#define DIMM_VRD2_CURR		0x3C
+#define RCA_VRD_CURR		0x3D
 
 struct smpro_hwmon {
 	struct regmap *regmap;
@@ -66,129 +66,129 @@ struct smpro_sensor {
 
 static const struct smpro_sensor temperature[] = {
 	{
-		.reg = SOC_TEMP_REG,
+		.reg = SOC_TEMP,
 		.label = "temp1 SoC"
 	},
 	{
-		.reg = SOC_VRD_TEMP_REG,
+		.reg = SOC_VRD_TEMP,
 		.label = "temp2 SoC VRD"
 	},
 	{
-		.reg = DIMM_VRD_TEMP_REG,
+		.reg = DIMM_VRD_TEMP,
 		.label = "temp3 DIMM VRD"
 	},
 	{
-		.reg = CORE_VRD_TEMP_REG,
+		.reg = CORE_VRD_TEMP,
 		.label = "temp4 CORE VRD"
 	},
 	{
-		.reg = CH0_DIMM_TEMP_REG,
+		.reg = CH0_DIMM_TEMP,
 		.label = "temp5 CH0 DIMM"
 	},
 	{
-		.reg = CH1_DIMM_TEMP_REG,
+		.reg = CH1_DIMM_TEMP,
 		.label = "temp6 CH1 DIMM"
 	},
 	{
-		.reg = CH2_DIMM_TEMP_REG,
+		.reg = CH2_DIMM_TEMP,
 		.label = "temp7 CH2 DIMM"
 	},
 	{
-		.reg = CH3_DIMM_TEMP_REG,
+		.reg = CH3_DIMM_TEMP,
 		.label = "temp8 CH3 DIMM"
 	},
 	{
-		.reg = CH4_DIMM_TEMP_REG,
+		.reg = CH4_DIMM_TEMP,
 		.label = "temp9 CH4 DIMM"
 	},
 	{
-		.reg = CH5_DIMM_TEMP_REG,
+		.reg = CH5_DIMM_TEMP,
 		.label = "temp10 CH5 DIMM"
 	},
 	{
-		.reg = CH6_DIMM_TEMP_REG,
+		.reg = CH6_DIMM_TEMP,
 		.label = "temp11 CH6 DIMM"
 	},
 	{
-		.reg = CH7_DIMM_TEMP_REG,
+		.reg = CH7_DIMM_TEMP,
 		.label = "temp12 CH7 DIMM"
 	},
 	{
-		.reg = RCA_VRD_TEMP_REG,
+		.reg = RCA_VRD_TEMP,
 		.label = "temp13 RCA VRD"
 	},
 };
 
 static const struct smpro_sensor voltage[] = {
 	{
-		.reg = CORE_VRD_VOLT_REG,
+		.reg = CORE_VRD_VOLT,
 		.label = "vout0 CORE VRD"
 	},
 	{
-		.reg = SOC_VRD_VOLT_REG,
+		.reg = SOC_VRD_VOLT,
 		.label = "vout1 SoC VRD"
 	},
 	{
-		.reg = DIMM_VRD1_VOLT_REG,
+		.reg = DIMM_VRD1_VOLT,
 		.label = "vout2 DIMM VRD1"
 	},
 	{
-		.reg = DIMM_VRD2_VOLT_REG,
+		.reg = DIMM_VRD2_VOLT,
 		.label = "vout3 DIMM VRD2"
 	},
 	{
-		.reg = RCA_VRD_VOLT_REG,
+		.reg = RCA_VRD_VOLT,
 		.label = "vout4 RCA VRD"
 	},
 };
 
 static const struct smpro_sensor curr_sensor[] = {
 	{
-		.reg = CORE_VRD_CURR_REG,
+		.reg = CORE_VRD_CURR,
 		.label = "iout1 CORE VRD"
 	},
 	{
-		.reg = SOC_VRD_CURR_REG,
+		.reg = SOC_VRD_CURR,
 		.label = "iout2 SoC VRD"
 	},
 	{
-		.reg = DIMM_VRD1_CURR_REG,
+		.reg = DIMM_VRD1_CURR,
 		.label = "iout3 DIMM VRD1"
 	},
 	{
-		.reg = DIMM_VRD2_CURR_REG,
+		.reg = DIMM_VRD2_CURR,
 		.label = "iout4 DIMM VRD2"
 	},
 	{
-		.reg = RCA_VRD_CURR_REG,
+		.reg = RCA_VRD_CURR,
 		.label = "iout5 RCA VRD"
 	},
 };
 
 static const struct smpro_sensor power[] = {
 	{
-		.reg = CORE_VRD_PWR_REG,
-		.reg_ext = CORE_VRD_PWR_MW_REG,
+		.reg = CORE_VRD_PWR,
+		.reg_ext = CORE_VRD_PWR_MW,
 		.label = "power1 CORE VRD"
 	},
 	{
-		.reg = SOC_PWR_REG,
-		.reg_ext = SOC_PWR_MW_REG,
+		.reg = SOC_PWR,
+		.reg_ext = SOC_PWR_MW,
 		.label = "power2 SoC"
 	},
 	{
-		.reg = DIMM_VRD1_PWR_REG,
-		.reg_ext = DIMM_VRD1_PWR_MW_REG,
+		.reg = DIMM_VRD1_PWR,
+		.reg_ext = DIMM_VRD1_PWR_MW,
 		.label = "power3 DIMM VRD1"
 	},
 	{
-		.reg = DIMM_VRD2_PWR_REG,
-		.reg_ext = DIMM_VRD2_PWR_MW_REG,
+		.reg = DIMM_VRD2_PWR,
+		.reg_ext = DIMM_VRD2_PWR_MW,
 		.label = "power4 DIMM VRD2"
 	},
 	{
-		.reg = RCA_VRD_PWR_REG,
-		.reg_ext = RCA_VRD_PWR_MW_REG,
+		.reg = RCA_VRD_PWR,
+		.reg_ext = RCA_VRD_PWR_MW,
 		.label = "power5 RCA VRD"
 	},
 };
@@ -208,14 +208,14 @@ static int smpro_read_temp(struct device *dev, u32 attr, int channel, long *val)
 		*val = (value & 0x1ff) * 1000;
 		break;
 	case hwmon_temp_crit:
-		if (temperature[channel].reg == SOC_VRD_TEMP_REG) {
-			ret = regmap_read(hwmon->regmap, SOC_VR_HOT_THRESHOLD_REG, &value);
+		if (temperature[channel].reg == SOC_VRD_TEMP) {
+			ret = regmap_read(hwmon->regmap, SOC_VR_HOT_THRESHOLD, &value);
 			if (ret)
 				return ret;
 			*val = (value & 0x1ff) * 1000;
 		} else {
 			/* Report same MEM HOT threshold across DIMM channels */
-			ret = regmap_read(hwmon->regmap, MEM_HOT_THRESHOLD_REG, &value);
+			ret = regmap_read(hwmon->regmap, MEM_HOT_THRESHOLD, &value);
 			if (ret)
 				return ret;
 			*val = (value & 0x1ff) * 1000;
